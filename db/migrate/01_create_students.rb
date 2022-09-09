61490e5e49b4887971e7b16d08993cf78c7e117d
Miriam <miriamjoshua00@gmail.com>
2022-09-09 13:50:12 -0400

    commit tree 7b8159fb87fc6864203b6942c9258cae66dab017

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,16 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
+
+
+# create_table :artists do |t|
+#   t.string :name
+#   t.string :genre
+#   t.integer :age
+#   t.string :hometown
+#   # the id column is generated automatically for every table! no need to specify it here.
+# end
